ae099a1a1200eea091545a7db8ca703b3f4d23bc
Mvuezana Emmanuel Exauce <mvuezanae@com.com>
2026-05-19 18:35:17 +0100

    fix: nettoyage complet de la config PWA pour le logo

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,6 +1,16 @@
 import { RouterProvider } from "react-router";
 import { router } from "./routes";
+import InstallPrompt from '../app/components/InstallPrompt';
+import React from "react";
 
 export default function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <>
+      {/* Gère toute l'application et les routes */}
+      <RouterProvider router={router} />
+      
+      {/* Écoute globalement l'événement PWA et affiche la bannière d'installation */}
+      <InstallPrompt />
+    </>
+  );
 }
