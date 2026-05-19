d494120d5d446395cdbe6ed35d46c4257ac3c5b8
Mvuezana Emmanuel Exauce <mvuezanae@com.com>
2026-05-19 17:46:43 +0100

    feat: ajout PWA installable et correction des routes Vercel

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import path from 'path'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
-
+import { VitePWA } from 'vite-plugin-pwa'
 
 function figmaAssetResolver() {
   return {
@@ -23,6 +23,33 @@ export default defineConfig({
     // Tailwind is not being actively used – do not remove them
     react(),
     tailwindcss(),
+    
+    // Ajout de la configuration pour rendre l'application installable (PWA)
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'maskable-icon.png'],
+      manifest: {
+        name: 'Masolo Chat',
+        short_name: 'Masolo',
+        description: 'Application de messagerie en temps réel',
+        theme_color: '#ffffff',
+        background_color: '#ffffff',
+        display: 'standalone', // Supprime la barre du navigateur sur téléphone
+        orientation: 'portrait',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
   ],
   resolve: {
     alias: {
